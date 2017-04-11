09fb83347d372f64b807cc88e63b95409b28cb23
jiangTaoQuite <jiangtao103cp@163.com>
2017-04-12 01:23:24 +0800

    fix

--- src/main/java/tech/jiangtao/backstage/swagger/Swagger.java
+++ src/main/java/tech/jiangtao/backstage/swagger/Swagger.java
@@ -1,18 +1,29 @@
 package tech.jiangtao.backstage.swagger;
 
+import com.google.common.collect.Lists;
+import io.swagger.models.Contact;
 import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.AlternateTypeRule;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.WildcardType;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
 
 /**
  * @class: Swagger </br>
@@ -35,7 +46,9 @@ public class Swagger {
         .paths(PathSelectors.any())
         .build()
         .apiInfo(apiInfo());
+
   }
+
 
 
   private ApiInfo apiInfo() {
