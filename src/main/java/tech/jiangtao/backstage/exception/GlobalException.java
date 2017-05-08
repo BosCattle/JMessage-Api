036b9bca912137b705d1ded804bdbd4566626b61
jiangTaoQuite <jiangtao103cp@163.com>
2017-05-09 01:29:13 +0800

    fix

---
+++ src/main/java/tech/jiangtao/backstage/exception/GlobalException.java
@@ -0,0 +1,16 @@
+package tech.jiangtao.backstage.exception;
+
+/**
+ * @class: GlobalException </br>
+ * @description: 全局一场类 </br>
+ * @creator: kevin </br>
+ * @email: jiangtao103cp@gmail.com </br>
+ * @date: 09/05/2017 01:25</br>
+ * @version: 0.0.1 </br>
+ **/
+public class GlobalException extends Exception {
+
+  public GlobalException(String exception, Throwable throwable) {
+
+  }
+}
